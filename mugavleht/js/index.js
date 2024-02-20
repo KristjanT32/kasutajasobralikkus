e5bcd747f2234b4599cc6298e4c916058d2f562f
Kristjan Taniel <kristjan.taniel@gmail.com>
2024-02-20 22:18:35 +0200

    UI work
    - Added an interaction blocker to stop those pesky users from using the website while a modal is shown
    - Repositioned the central text
    - Implemented support for defined modals

--- mugavleht/js/index.js
+++ mugavleht/js/index.js
@@ -1,16 +1,22 @@
-let modal = document.querySelector(".modal-popup");
-let modalTitle = document.querySelector(".modal-popup .title");
-let modalDesc = document.querySelector(".modal-popup .description");
-let modalDismiss = document.querySelector(".modal-popup .modal-dismiss-button");
+const modal = document.querySelector(".modal-popup");
+const modalTitle = document.querySelector(".modal-popup .title");
+const modalDesc = document.querySelector(".modal-popup .description");
+const modalDismiss = document.querySelector(
+  ".modal-popup .modal-dismiss-button"
+);
+const modalArea = document.querySelector(".modal-popup-area");
+const interactionBlocker = document.querySelector(".interaction-blocker");
 
-let modalContent = document.querySelector(".modal-popup .modal-content");
-let modalClosing = document.querySelector(".modal-popup .modal-closing");
+const modalContent = document.querySelector(".modal-popup .modal-content");
+const modalClosing = document.querySelector(".modal-popup .modal-closing");
 
 let timerLabel = document.querySelector(
   ".modal-popup .estimated-processing-time"
 );
 let loadingStatus = document.querySelector(".modal-popup .loading-status");
+
 let modalShown = false;
+let secondaryModalShown = false;
 
 let currentModalTimer = 0;
 let timerRunning = false;
@@ -24,6 +30,30 @@ const loadingPhrases = [
   "Analüüsime vastust...",
 ];
 
+const modals = [
+  `<div class="modal-popup-defined">
+  <div class="modal-dismiss-button" onclick='hideDefinedModal()'>
+    <i
+      class="fas fa-times"
+      style="font-family: 'Font Awesome 5 Solid'"
+    ></i>
+  </div>
+  <div class="text-container">
+  <img
+      src="/mugavleht/assets/logo.png"
+      alt="SBE logo"
+      width="100px"
+      height="100px"
+  />
+    <div class="modal-content">
+      <div class="title" style='color:red'>{title}</div>
+      <div class="description">{description}</div>
+      <br style="margin-top: 20px" />
+    </div>
+  </div>
+</div>`,
+];
+
 modalDismiss.addEventListener("click", () => {
   modalContent.style.display = "none";
   modalClosing.style.display = "block";
@@ -31,7 +61,7 @@ modalDismiss.addEventListener("click", () => {
 });
 
 /**
- * Show a modal dialog to the user.
+ * Show the default modal dialog to the user.
  * @param {string} title The title of the modal dialog.
  * @param {string} desc The description text of the modal dialog.
  */
@@ -54,6 +84,36 @@ function showModal(title, desc) {
   if (!modal.classList.contains("modal-popup-show")) {
     modal.classList.add("modal-popup-show");
   }
+  interactionBlocker.style.display = "block";
+}
+
+/**
+ *
+ * @param {int} modalID - the ID of the modal type to use
+ * @param {object} settings - the settings for the modal
+ */
+function showDefinedModal(modalID, { title, desc, bt1, bt2, cb1, cb2 }) {
+  if (secondaryModalShown) {
+    return;
+  }
+
+  if (modals[modalID] == undefined) {
+    log(
+      "ModalID " +
+        modalID +
+        " is invalid. The largest available index is " +
+        (modals.length - 1)
+    );
+    return;
+  }
+
+  modalArea.insertAdjacentHTML(
+    "beforeend",
+    modals[modalID]
+      .replaceAll("{title}", title ? title : "")
+      .replaceAll("{description}", desc ? desc : "")
+  );
+  secondaryModalShown = true;
 }
 
 /**
@@ -71,6 +131,24 @@ function hideModal() {
   }
 
   modalShown = false;
+  interactionBlocker.style.display = "none";
+}
+
+function hideDefinedModal() {
+  if (!secondaryModalShown) return;
+  document
+    .querySelector(".modal-popup-defined")
+    .classList.remove("modal-popup-show");
+  document
+    .querySelector(".modal-popup-defined")
+    .classList.add("modal-popup-hide");
+  setTimeout(() => {
+    document
+      .querySelector(".modal-popup-defined")
+      .classList.remove("modal-popup-hide");
+    document.querySelector(".modal-popup-defined").remove();
+  }, 1000);
+  secondaryModalShown = false;
 }
 
 /**
@@ -113,4 +191,32 @@ function formatTime(seconds) {
     ":" +
     (secs <= 9 ? "0" + secs : secs)
   );
+}
+
+/**
+ * Logs the provided string to the console.
+ * This function automatically determines, whether the logged message is an error or not based on the content.
+ * @param {string} str The message to log.
+ */
+function log(str) {
+  let errorWords = [
+    "error",
+    "failed",
+    "couldn't",
+    "could not",
+    "err",
+    "failure",
+    "not found",
+    "invalid",
+  ];
+  if (
+    str
+      .toLowerCase()
+      .split(" ")
+      .filter((word) => errorWords.includes(word)).length > 0
+  ) {
+    console.error("[ERROR]: " + str);
+  } else {
+    console.info("[INFO]: " + str);
+  }
 }
